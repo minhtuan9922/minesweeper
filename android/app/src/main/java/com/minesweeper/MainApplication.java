e85b3a5342c95b35745f5d7b5fb9bb7660eb5048
Tuan Pham <tuanpham@asia.icsc.vn>
2023-03-10 17:32:05 +0700

    update

--- android/app/src/main/java/com/minesweeper/MainApplication.java
+++ android/app/src/main/java/com/minesweeper/MainApplication.java
@@ -7,7 +7,9 @@ import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.react.ReactPackage;
+import com.minesweeper.newarchitecture.MainApplicationReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -35,16 +37,23 @@ public class MainApplication extends NavigationApplication {
           return "index";
         }
       };
+  private final ReactNativeHost mNewArchitectureNativeHost =
+      new MainApplicationReactNativeHost(this);
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      return mNewArchitectureNativeHost;
+    } else {
+      return mReactNativeHost;
+    }
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    
+    // If you opted-in for the New Architecture, we enable the TurboModule system
+    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
